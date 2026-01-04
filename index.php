a4a5eb46d4fbd61a41703557f4f39fb3a4002bd2
Max <100712759+mbgl20@users.noreply.github.com>
2026-01-04 23:10:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
-
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="UTF-8">
+		<title>free-fa</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php include 'fafree.php'; ?>
+		<style>
+			code {
+				color: #333;
+				background: silver;
+			}
+			img {
+				border: 1px solid #333;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Free-FA Demo</h1>
+		<table>
+			<tr>
+				<td><i class="fa-chisel fa-regular fa-microphone"></i></td>
+				<td><i class="fa-chisel fa-regular fa-microphone-slash"></i></td>
+				<td><i class="fa-chisel fa-regular fa-phone"></i></td>
+				<td><i class="fa-chisel fa-regular fa-phone-slash"></i></td>
+				<td><i class="fa-sharp-duotone fa-solid fa-donut"></i></td>
+				<td><i class="fa-duotone fa-solid fa-mobile-button"></i></td>
+			</tr>
+			<tr>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/1.png"></td>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/2.png"></td>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/3.png"></td>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/4.png"></td>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/5.png"></td>
+				<td><img src="https://cdn.mabgl.com/germany/mbdemo/fa-free/6.png"></td>
+			</tr>
+			<tr>
+				<td><code>&lt;i class="fa-chisel fa-regular fa-microphone"&gt;&lt;/i&gt;</code></td>
+				<td><code>&lt;i class="fa-chisel fa-regular fa-microphone-slash"&gt;&lt;/i&gt;</code></td>
+				<td><code>&lt;i class="fa-chisel fa-regular fa-phone"&gt;&lt;/i&gt;</code></td>
+				<td><code>&lt;i class="fa-chisel fa-regular fa-phone-slash"&gt;&lt;/i&gt;</code></td>
+				<td><code>&lt;i class="fa-sharp-duotone fa-solid fa-donut"&gt;&lt;/i&gt;</code></td>
+				<td><code>&lt;i class="fa-duotone fa-solid fa-mobile-button"&gt;&lt;/i&gt;</code></td>
+			</tr>
+		</table>
+	</body>
+</html>
